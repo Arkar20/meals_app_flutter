f51bf6b2e30123df7775e04c24f1e760360e0ea0
Arkar <69750819+Arkar20@users.noreply.github.com>
2023-07-03 13:14:28 +0630

    adding filter list

--- lib/screens/filter.dart
+++ lib/screens/filter.dart
@@ -8,8 +8,48 @@ class FilterScreen extends StatefulWidget {
 }
 
 class _FilterScreenState extends State<FilterScreen> {
+  var glutenFree = false;
+  var isVegan = false;
+
+  void handleChangeGluten(isChecked) {
+    setState(() {
+      glutenFree = isChecked;
+    });
+  }
+
+  void handleChangeVegan(isChecked) {
+    setState(() {
+      isVegan = isChecked;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Fliter Setting"),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: glutenFree,
+            onChanged:handleChangeGluten,
+            title: Text("Gluten free",style: Theme.of(context).textTheme.titleMedium),
+            subtitle: Text('Only Include gluten free meals',style: Theme.of(context).textTheme.bodySmall),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: EdgeInsets.symmetric(horizontal: 24),
+          ),
+          SwitchListTile(
+            value: isVegan,
+            onChanged:handleChangeVegan,
+            title: Text("Vegetarians",style: Theme.of(context).textTheme.titleMedium),
+            subtitle: Text('Only Include gluten free meals',style: Theme.of(context).textTheme.bodySmall),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: EdgeInsets.symmetric(horizontal: 24),
+          ),
+         
+        ],
+      ),
+    );
   }
 }
